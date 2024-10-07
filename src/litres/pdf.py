9c181f3de66c9e179303ae6b443687184bd364b9
tans1q <rkharisov.main@gmail.com>
2024-10-07 20:36:21 +0200

    Added Litres crawler for text docs

--- src/litres/pdf.py
+++ src/litres/pdf.py
@@ -3,18 +3,16 @@ import json
 import os.path
 from urllib.parse import urlparse, parse_qs
 
+import pymupdf
 import requests
 from PIL import Image
 from rich.progress import track
-from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-from seleniumwire import webdriver
-import pymupdf
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from consts import domain
-
-
-from utils import get_sid, get_in_workdir
+from utils import get_sid, get_in_workdir, create_driver
 
 
 def visit_pdf_books_pages():
@@ -23,26 +21,32 @@ def visit_pdf_books_pages():
 
     Visit pdf books pages, download their pages separately and create pdf from them
     """
-    path_to_idx = get_in_workdir("books-index.json")
+    path_to_idx = get_in_workdir("artifacts/books-index.json")
     with open(path_to_idx, "r") as f:
-        books = json.load(f)
+        all_books = json.load(f)
 
-    books = [book for book in books if book['content_type'] == 'pdf']
+    pdf_books = [b for b in all_books.values() if b['content_type'] == 'pdf']
 
-    print(f"Visiting {len(books)} pdf books pages")
+    print(f"Visiting {len(pdf_books)} pdf books pages")
 
-    for book in books[:1]:
+    for book in pdf_books[:]:
         url = book['url']
         print(f"Visiting book page: {url}")
-        file_id = book.get('file_id') or _get_file_id(url)
-        page_extensions = book.get('ext') or _get_page_extensions(file_id)
-        _download_book_pages(file_id, page_extensions)
-        _create_pdf(file_id)
-        book['file_id'] = file_id
-        book['ext'] = page_extensions
+        try:
+            file_id = book.get('file_id') or _get_file_id(url)
+            page_extensions = book.get('ext') or _get_page_extensions(file_id)
+            _download_book_pages(file_id, page_extensions)
 
-        with open(path_to_idx, "w") as f:
-            json.dump(books, f, indent=4, ensure_ascii=False)
+            book['file_id'] = file_id
+            book['ext'] = page_extensions
+            with open(path_to_idx, "w") as f:
+                json.dump(all_books, f, indent=4, ensure_ascii=False)
+
+            _create_pdf(book)
+        except Exception as e:
+            print(f"Error occurred while processing book: {url}")
+            print(e)
+
 
 def _get_file_id(book_page_url):
     """
@@ -51,17 +55,20 @@ def _get_file_id(book_page_url):
     :param book_page_url:
     :return: internal file id
     """
-    print(f"Getting file_id for book page: {book_page_url}")
-    with _create_driver() as driver:
+    print(f"Getting file id for book page: {book_page_url}")
+    with create_driver() as driver:
         driver.get(book_page_url)
-        read_button = driver.find_element(value='div[class^="Button_textContainer__"]', by=By.CSS_SELECTOR)
+        read_button = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'div[class^="Button_textContainer__"]'))
+        )
         read_button.click()
         reader_url = driver.current_url
         parsed_url = urlparse(reader_url)
         queries = parse_qs(parsed_url.query)
         if not (file := queries.get('file')):
-            raise ValueError(f"Could not find file_id in url: {reader_url}")
+            raise ValueError(f"Could not find file id in url: {reader_url}")
         return file[0]
+
 
 def _get_page_extensions(file_id):
     """
@@ -76,69 +83,59 @@ def _get_page_extensions(file_id):
     }
     with requests.get(f"{domain}/pages/get_pdf_js/?file={file_id}", headers=headers) as r:
         r.raise_for_status()
-        with _create_driver() as driver:
-            return driver.execute_script("""let PFURL = { pdf: { } };""" + r.text+"; return PFURL.pdf[" + file_id + "];")
+        with create_driver() as driver:
+            return driver.execute_script(
+                """let PFURL = { pdf: { } };""" + r.text + "; return PFURL.pdf[" + file_id + "];")
 
 
 def _download_book_pages(file_id, page_extensions):
     """
     Download all pages for the book
     """
-    artifacts_dir = os.path.join("artifacts", "litres", file_id)
+    artifacts_dir = get_in_workdir(os.path.join("artifacts", "images", file_id))
 
-    if not os.path.exists(artifacts_dir):
-        os.makedirs(artifacts_dir, exist_ok=True)
+    os.makedirs(artifacts_dir, exist_ok=True)
 
     p = page_extensions['pages'][0]['p']
 
-    for page_no in track(range(len(p)), description=f"Downloading pages for file: {file_id}"):
+    for page_no in track(range(0, len(p)), description=f"Downloading pages for file: {file_id}"):
         ext = p[page_no]['ext']
+        w = p[page_no]['w']
         file_name = os.path.join(artifacts_dir, f"{page_no}.{ext}")
         if not os.path.exists(file_name):
-            url = f"{domain}/pages/get_pdf_page/?file={file_id}&page={page_no}&rt=w1900&ft={ext}"
+            url = f"{domain}/pages/get_pdf_page/?file={file_id}&page={page_no}&rt=w{w}&ft={ext}"
             headers = {
                 "Cookie": f"SID={get_sid()};"
             }
             with requests.get(url, headers=headers, stream=True) as r:
                 r.raise_for_status()
                 with Image.open(io.BytesIO(r.content)) as img:
-                    img.save(file_name, quality=95, optimize=True, progressive=True, resolution=300, subsampling=0)
+                    img.save(file_name, quality=95, optimize=True, subsampling=0)
 
 
-def _create_pdf(file_id):
+def _create_pdf(book):
     """
     Create pdf from downloaded pages images
     """
-    artifacts_dir = os.path.join("artifacts", "litres", file_id)
-    pdf_file = os.path.join("artifacts", "litres", f"{file_id}.pdf")
+    file_id = book['file_id']
+    artifacts_dir = get_in_workdir(os.path.join("artifacts", "images", file_id))
+    pdf_dir = get_in_workdir("artifacts/docs")
+    os.makedirs(pdf_dir, exist_ok=True)
+
+    name_with_ext = f"{book['full_name']}.pdf"
+    pdf_file = os.path.join(pdf_dir, name_with_ext)
+    if os.path.exists(pdf_file):
+        return
+
     with pymupdf.open() as doc:
         # sort pages by number
-        images = sorted([f for f in os.listdir(artifacts_dir) if not f.endswith('.tmp')], key=lambda x: int(x.split(".")[0]))
+        images = sorted([f for f in os.listdir(artifacts_dir)], key=lambda x: int(x.split(".")[0]))
         for page in track(images, description=f"Creating pdf for file: {file_id}"):
             with pymupdf.open(os.path.join(artifacts_dir, page)) as img:
                 rect = img[0].rect  # pic dimension
-                img_pdf = pymupdf.open("pdf",  img.convert_to_pdf() )  # open stream as PDF
+                img_pdf = pymupdf.open("pdf", img.convert_to_pdf())  # open stream as PDF
                 page = doc.new_page(width=rect.width, height=rect.height)
                 page.show_pdf_page(rect, img_pdf, 0)  # image fills the page
 
         doc.save(pdf_file)
 
-
-def _create_driver():
-    """
-    Create a new Selenium driver instance with request interceptor
-    """
-    def _interceptor(request):
-        # add the missing headers
-        request.headers['Cookie'] = f"SID={get_sid()};"
-
-    options = webdriver.ChromeOptions()
-    options.headless = True
-    options.add_argument("--headless")
-
-    driver = webdriver.Chrome(
-        service=ChromeService(ChromeDriverManager().install()),
-        options=options
-    )
-    driver.request_interceptor = _interceptor
-    return driver
--- src/litres/utils.py
+++ src/litres/utils.py
@@ -1,31 +1,67 @@
 import os.path
 
-import yaml
 import requests
+import yaml
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from seleniumwire import webdriver
+import hashlib
 
 def get_in_workdir(file):
     """Return file in the current directory where script file is located"""
     return os.path.join(os.path.dirname(__file__), file)
+
 
 def read_config():
     # read config file from the same directory
     with open(get_in_workdir("config.yaml"), "r") as f:
         return yaml.safe_load(f)
 
+
 def get_sid():
     config = read_config()
-    if 'sid' in config:
-        return config['sid']
+    if sid := config.get('sid'):
+        return sid
     auth_url = "https://api.litres.ru/foundation/api/auth/login"
     data = {
         "login": "rkharisov.main@gmail.com",
         "password": "wfh4d4x9",
     }
+    if not (app_id := config['app-id']):
+        raise ValueError("app-id is not set in config")
     auth_headers = {
-        "app-id": read_config()['app-id'],
+        "app-id": app_id,
     }
     with requests.post(auth_url, json=data, headers=auth_headers) as r:
         r.raise_for_status()
         sid = r.json()['payload']['data']['sid']
         print(f"Requested new SID: {sid}, consider updating config")
         return sid
+
+def create_driver():
+    """
+    Create a new Selenium driver instance with request interceptor
+    """
+
+    def _interceptor(request):
+        # add the missing headers
+        request.headers['Cookie'] = f"SID={get_sid()};"
+
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    options.add_argument("--headless")
+
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()),
+        options=options
+    )
+    driver.request_interceptor = _interceptor
+    return driver
+
+def get_hash(src):
+    """
+    get MD5 hash of the source text
+    :param src: bytes to calculate hash
+    :return: hex digest of the hash
+    """
+    return hashlib.md5(src.encode()).hexdigest()
