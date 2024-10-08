4eaa4763751c7695e627f540fd1afbdeb9c40e3b
tans1q <rkharisov.main@gmail.com>
2024-10-08 22:47:56 +0200

    WIP

--- litres/upload_books.py
+++ litres/upload_books.py
@@ -1,6 +1,13 @@
 import json
+import os.path
 
-from utils import get_in_workdir
+from monocorpus_models import Session, Document
+from rich.progress import track
+from yadisk_client import YaDisk
+
+from utils import get_in_workdir, read_config
+
+REMOTE_DIR = "/НейроТатарлар/kitaplar/_все книги/litres"
 
 
 def upload_pdfs():
@@ -8,8 +15,38 @@ def upload_pdfs():
     with open(_index_file, "r") as f:
         _all_docs = json.load(f)
 
-    for doc in [d for d in _all_docs.values() if d['content_type'] == 'pdf'][:1]:
-        print(f"Uploading book: {doc['full_name']}")
-        # upload_book(doc)
-        # create_public_link(doc)
-        # save_to_google_sheets(doc)
+    config = read_config()
+    client = YaDisk(config['yandex.oauth_token'])
+    session = Session()
+
+    for doc in track([d for d in _all_docs.values() if d['content_type'] == 'pdf'],
+                     description="Uploading documents"):
+        file = doc['pdf_file']
+        remote_path, md5 = client.upload_or_replace(file, remote_dir=REMOTE_DIR)
+        client.publish(remote_path, fields=['public_key', 'public_url'])
+
+        meta = client.get_meta(remote_path, fields=['public_key', 'public_url', 'resource_id'])
+        ya_public_url = meta['public_url']
+        ya_public_key = meta['public_key']
+        ya_resource_id = meta['resource_id']
+
+        _metadata = doc['metadata']
+        document = Document(
+            md5=md5,
+            mime_type='application/pdf',
+            names=os.path.basename(file),
+            ocr='required',
+            ya_public_url=ya_public_url,
+            ya_public_key=ya_public_key,
+            ya_resource_id=ya_resource_id,
+            publisher=_metadata.get('publisher'),
+            author=doc.get('author') or _metadata.get('created_by'),
+            title=doc.get('title'),
+            age_limit=_metadata.get('age_limit'),
+            isbn=_metadata.get('isbn'),
+            publish_date=_metadata.get('publish_date'),
+            summary=_metadata.get('summary'),
+            sources=doc.get('url'),
+        )
+        session.upsert(document)
+        print(f"Document uploaded: {md5}")
