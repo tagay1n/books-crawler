5a0bfe3bf0d3a004b912048d6c0cdbe8ca81e45c
tagay1n <rkharisov.tt@gmail.com>
2025-05-04 10:17:22 +0300

    another scrapping episode + changed to upload doc and meta to s3 and do not upload meta to yadisk

--- milli_kitaphana/upload_docs.py
+++ milli_kitaphana/upload_docs.py
@@ -1,27 +1,43 @@
-import json
 import os.path
-import re
+from boto3 import Session
 
-from monocorpus_models import Session, Document
-from rich.progress import track
-from sqlalchemy import text
+
 from yadisk_client import YaDisk, ConflictResolution
-from rich import print
-
-REMOTE_DIR = "/НейроТатарлар/kitaplar/_все книги/милли.китапханә"
 
 
-def upload_docs(path_to_pdf, path_to_texts_zip, path_to_metadata, config, is_limited):
-    remote_dir = os.path.join(REMOTE_DIR, "limited" if is_limited else "full", os.path.splitext(os.path.basename(path_to_pdf))[0][:100])
-    client = YaDisk(config['yandex.oauth_token'])
+def upload_doc(path_to_pdf, config, is_limited):
+    remote_dir = os.path.join(config['yandex']['disk']['target_dir'], "limited" if is_limited else "full")
+    client = YaDisk(config['yandex']['disk']['oauth_token'])
 
-    for f in [path_to_pdf, path_to_texts_zip, path_to_metadata]:
-        if not f:
-            continue
-        remote_path, _ = client.upload_or_replace(
-            f, 
-            remote_dir=remote_dir,
-            conflict_resolution=ConflictResolution.SKIP
+    remote_path, _ = client.upload_or_replace(
+        path_to_pdf, 
+        remote_dir=remote_dir,
+        conflict_resolution=ConflictResolution.SKIP
+    )
+    res = client.publish(remote_path)
+    res = client.get_meta(res.path, fields=['md5'])
+    return res.md5
+    
+def upload_metadata(path_to_metadata, path_to_pdf, context):
+    config = context['config']
+    client = Session().client(
+        service_name='s3',
+        aws_access_key_id=config['yandex']['cloud']['aws_access_key_id'],
+        aws_secret_access_key=config['yandex']['cloud']['aws_secret_access_key'],
+        endpoint_url='https://storage.yandexcloud.net'
+    )
+    meta_key = f"{context['md5']}.zip"
+    meta_bucket = config["yandex"]["cloud"]['bucket']['upstream_metadata']
+    client.upload_file(
+        path_to_metadata,
+        meta_bucket,
+        meta_key
+    )
+    doc_key = f"{context['md5']}.pdf"
+    doc_bucket = config["yandex"]["cloud"]['bucket']['document']
+    if not client.list_objects_v2(Bucket=doc_bucket, Prefix=doc_key, MaxKeys=1).get("Contents", []):
+        client.upload_file(
+            path_to_pdf,
+            doc_bucket,
+            doc_key
         )
-        client.publish(remote_path)
-
