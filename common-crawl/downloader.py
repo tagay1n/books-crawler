ec20027c94942f6166518feffd8655d107e5114c
tagay1n <rkharisov.tt@gmail.com>
2025-09-10 10:59:43 +0300

    common-crawler wip

--- common-crawl/downloader.py
+++ common-crawl/downloader.py
@@ -6,7 +6,7 @@ import pymupdf
 import pandas as pd
 import hashlib
 import shutil
-from urllib.parse import urlparse
+from urllib.parse import urlparse, unquote
 from rich.progress import track
 import re
 import datetime
@@ -14,12 +14,74 @@ from warcio.archiveiterator import ArchiveIterator
 
 
 def download():
+    # download_directly()
+    download_cc()
+    
+    
+def download_cc():
+    index = _load_index()
+    _dump_index(index=index, backup=True)
+    try: 
+        _augment(index)
+        return
+        docs_to_download = [d for d in index.values() if not d.get('path_to_file', None)]
+        print(f"About to download {len(docs_to_download)} of {len(index)} documents")
+        path_to_file = _get_in_workdir("../__artifacts/common.crawl/tmp.pdf")
+
+        for doc in docs_to_download:
+                warc_file = doc['file_path'] if not doc['file_path'].endswith('.parquet') else doc.get('augmented_file_path', None)
+                if not warc_file:
+                    continue
+                print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+                if _fetch_record_from_offset(warc_file, doc['offset'], path_to_file) and check_pdf(path_to_file):
+                    doc['md5'] = _calculate_md5(path_to_file)
+                    doc['path_to_file'] = _move_file(path_to_file, doc['md5'], doc['url'], doc['script'])
+                    print(f"Saved doc '{doc['md5']}' to '{doc['path_to_file']}'")
+                    _dump_index(index=index)
+                else:
+                    print(f"Could not download doc from common crawl", doc)
+                    continue
+    except KeyboardInterrupt:
+        print("Interrupting...")
+    finally:
+        _dump_index(index=index)
+
+
+def _augment(index):
+    docs_to_augment = [d for d in index.values() if (not d.get('path_to_file', None)) and (not d.get('augmented_file_path', None)) and d['file_path'].endswith('.parquet')]
+    index_file_to_docs = {}
+    for doc in docs_to_augment:
+        warc_filename = doc['file_path'].strip()
+        # url = warc_filename.replace("s3://commoncrawl/", "https://data.commoncrawl.org/")
+        # warc_id = _get_warc_id(warc_filename)
+        if warc_filename in index_file_to_docs:
+            index_file_to_docs[warc_filename].append(doc)
+        else: 
+            index_file_to_docs[warc_filename] = [doc]
+    
+    
+    for warc_filename, docs in index_file_to_docs.items():
+        try:
+            df = _get_warc(warc_filename)
+            for doc in docs:
+                aug_warc_filename =  _look_for_filename_in_index(doc, df)
+                if aug_warc_filename:
+                    doc['augmented_file_path'] = aug_warc_filename
+            _dump_index(index=index)  
+            del df
+        except Exception as e:
+            print(f"Error: {e}")
+            continue
+
+        
+
+def download_directly():
     index = _load_index()
     _dump_index(index=index, backup=True)
     try: 
         docs_to_download = [d for d in index.values() if not d.get('path_to_file', None)]
-        path_to_file = _get_in_workdir("../__artifacts/common.crawl/tmp.pdf")
-        slice_size = 500
+        path_to_file = _get_in_workdir(f"../__artifacts/common.crawl/tmp.pdf")
+        slice_size = 25
         start = 0
         while True:
             slice = docs_to_download[start:start+slice_size]
@@ -30,17 +92,13 @@ def download():
             for doc in track(slice, "Downlading document..."):
                 print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
                 if _download_direct_pdf(doc['url'], path_to_file) and check_pdf(path_to_file):
-                    pass
-                elif _fetch_record_from_offset(doc['file_path'], doc['url'], doc['offset'], path_to_file) and check_pdf(path_to_file):
-                    pass
+                    doc['md5'] = _calculate_md5(path_to_file)
+                    doc['path_to_file'] = _move_file(path_to_file, doc['md5'], doc['url'], doc['script'])
+                    print(f"Saved doc '{doc['md5']}' to '{doc['path_to_file']}'")
+                    _dump_index(index=index)
                 else:
-                    print(f"Could not download doc from any of resources", doc)
+                    print(f"Could not download doc directly", doc)
                     continue
-                doc['md5'] = _calculate_md5(path_to_file)
-                doc['path_to_file'] = _move_file(path_to_file, doc['md5'], doc['url'], doc['script'])
-                print(f"Saved doc '{doc['md5']}' to '{doc['path_to_file']}'")
-                _dump_index(index=index)
-             
     except KeyboardInterrupt:
         print("Interrupting...")
     finally:
@@ -74,35 +132,25 @@ def _calculate_md5(file_path: str):
     return hash_md5.hexdigest()
         
     
-def _look_for_filename_in_index(warc_url, orig_url):
-    df = _get_warc(warc_url)
-
-    exact_match = df[df['url'] == orig_url]
+def _look_for_filename_in_index(doc, df):
+    orig_url = doc['url']
+    exact_match = df[df['url'] == orig_url.strip()]
 
     if len(exact_match) == 1:
         return exact_match['warc_filename'].values[0]
+    elif len(exact_match) > 1:
+        print(f"Got more than one matches by url {orig_url}")
     else:
         print(f"File not found in index by url {orig_url}")
-        return None
-
-    # Show a few URLs
-    print(exact_match[["url"]].head())
-    print(exact_match.columns.values.tolist())
+    return None
     
-    return False
 
-
-def _fetch_record_from_offset(warc_filename, orig_url, offset, output_file):
+def _fetch_record_from_offset(warc_filename, offset, output_file):
     """
     Fetches a single WARC record from Common Crawl by offset only.
     Stops after the first record.
     """
     print(f"Trying do download doc from warc {warc_filename}")
-    
-    if warc_filename.endswith('.parquet'):
-        warc_filename = _look_for_filename_in_index(warc_filename, orig_url)
-        if not warc_filename:
-            return False
     
     try: 
         warc_url = warc_filename.removeprefix("s3://commoncrawl/")
@@ -126,21 +174,31 @@ def _fetch_record_from_offset(warc_filename, orig_url, offset, output_file):
     return False
     
     
-def _get_warc(url):
-    url = url.replace("s3://commoncrawl/", "https://data.commoncrawl.org/")
-    warc_id = _get_warc_id(url)
+def _get_warc(warc_filename, chunk_size: int = 8192):
     warcs_dir = os.path.normpath(_get_in_workdir(os.path.expanduser(f"~/.common-crawl/warcs")))
     os.makedirs(warcs_dir, exist_ok=True)
-    warc_path = os.path.join(warcs_dir, f"{warc_id}.parquet")
+    warc_path = os.path.join(warcs_dir, warc_filename.replace('/', '_'))
     
     if not os.path.exists(warc_path):
-        print(f"📥 Downloadin {url} ...")
-        df = pd.read_parquet(url)
-        df.to_parquet(warc_path)   # Save locally
+        https_url = warc_filename.replace("s3://commoncrawl/", "https://data.commoncrawl.org/")
+        tmp_warc_path = f"{warc_path}.part"
+        # Stream download to avoid loading entire file into memory
+        with requests.get(https_url, stream=True) as r:
+            r.raise_for_status()
+            total_size = int(r.headers.get("Content-Length", 0))
+            with open(tmp_warc_path, "wb") as f:
+                # Use track to visualize progress
+                for chunk in track(
+                    r.iter_content(chunk_size=chunk_size),
+                    description=f"Downloading '{https_url[-100:]}'",
+                    total=total_size // chunk_size if total_size else None,
+                ):
+                    if chunk:  # filter out keep-alive chunks
+                        f.write(chunk)
+        shutil.move(tmp_warc_path, warc_path)
     else:
         print(f"📂 Using cached {warc_path}")
-        df = pd.read_parquet(warc_path)
-    return df
+    return pd.read_parquet(warc_path)
     
     
 def _get_warc_id(url):
@@ -152,21 +210,24 @@ def _get_warc_id(url):
 
         
 def check_pdf(path_to_file):
-    with pymupdf.open(path_to_file) as pdf_file:
-        for _ in pdf_file:
-            pass
-        
-        # not the best way but should work
-        if (not pdf_file.can_save_incrementally) or pdf_file.is_repaired:
-            print(f"Document {path_to_file} seems to be broken")
-            return False
+    try: 
+        with pymupdf.open(path_to_file) as pdf_file:
+            for _ in pdf_file:
+                pass
+            
+            # not the best way but should work
+            # if (not pdf_file.can_save_incrementally) or pdf_file.is_repaired:
+            #     print(f"Document {path_to_file} seems to be broken")
+            #     return False
+    except Exception:
+        return False
     return True
        
     
 def _download_direct_pdf(url, path_to_file):
     print(f"Trying do download doc by url {url}")
     try: 
-        response = requests.get(url, timeout=3, stream=True)
+        response = requests.get(url, timeout=3, stream=True, verify=False)
         
         if response.status_code == 200:
             with open(path_to_file, "wb") as f:
--- common-crawl/upload.py
+++ common-crawl/upload.py
@@ -5,7 +5,7 @@ from yadisk_client import YaDisk, ConflictResolution
 import os
 from rich.progress import track
 
-token = "<<SET ME>>>"
+token = "<<SET ME>>"
 upload_dir = "/НейроТатарлар/kitaplar/common_crawl"
 
 
@@ -21,7 +21,7 @@ def upload():
                 doc['uploaded'] = True
                 print(f"Doc {doc['md5']} uploaded")
                 _dump_index(original_index)
-                exit()
+                os.remove(doc['path_to_file'])
     except Exception as e:
         print(f"Error: {e}")
     finally:
@@ -29,14 +29,14 @@ def upload():
             
 
 def _upload_doc(ya_client, path_to_pdf):
-    remote_path, _ = ya_client.upload_or_replace(
+    _, _ = ya_client.upload_or_replace(
         path_to_pdf, 
         remote_dir=upload_dir,
         conflict_resolution=ConflictResolution.SKIP
     )
-    res = ya_client.publish(remote_path)
-    res = ya_client.get_meta(res.path, fields=['md5'])
-    return res.md5
+    # res = ya_client.publish(remote_path)
+    # res = ya_client.get_meta(res.path, fields=['md5'])
+    # return res.md5
     
     
 def _dedup(index):
@@ -46,8 +46,15 @@ def _dedup(index):
         all_md5s = gsheet_session.query(statement)
         all_md5s = set(all_md5s)
     
+    # keep only with md5
     dowloaded_docs_idx =  {k: v for k,v in index.items() if v.get('md5')}
+    # keep only not in yandex disk
     filtered_docs_idx = {k: v for k,v in dowloaded_docs_idx.items() if v['md5'] not in all_md5s}
+    _md5s_to_urns = {v['md5']: k for k, v in filtered_docs_idx.items()}
+    # remove duplicates in the downloaded docs
+    if len(_md5s_to_urns) != len(dowloaded_docs_idx):
+        print(f"There are duplicates in downloaded files: {len(_md5s_to_urns)} -- {len(dowloaded_docs_idx)}")
+        filtered_docs_idx = {urn: filtered_docs_idx[urn] for _, urn in _md5s_to_urns.items()}
     
     print(f"Total index {len(index)}, downloaded docs: {len(dowloaded_docs_idx)}, filtered_docs: {len(filtered_docs_idx)}")
     return filtered_docs_idx
